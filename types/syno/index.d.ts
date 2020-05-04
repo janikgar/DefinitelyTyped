57f2ea132e4bd3be298d5ba311c1810aae36f500
J. Garcia <janikgar@gmail.com>
2020-05-03 17:51:37 -0700

    combine to one file

--- types/syno/index.d.ts
+++ types/syno/index.d.ts
@@ -3,31 +3,149 @@
 // Definitions by: J. Garcia <https://github.com/janikgar>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/*~ If this module has methods, declare them as functions like so.
- */
-export function myMethod(a: string): string;
-export function myOtherMethod(a: number): number;
+import request = require('request');
+import fs = require('fs');
+import path = require('path');
+import _ = require('lodash');
 
-/*~ You can declare types that are available via importing the module */
-export interface someType {
-    name: string;
-    length: number;
-    extras?: string[];
+declare class Syno {
+    ss: SurveillanceStation;
+    dtv: VideoStationDTV;
+    vs: VideoStation;
+    as: AudioStation;
+    dl: DownloadStation;
+    fs: FileStation;
+    dsm: DSM;
+    auth: Auth;
+    session: null;
+    request: any;
+
+    constructor(params: {
+        account: string,
+        passwd: string,
+        protocol?: string,
+        host?: string,
+        port?: string,
+        apiVersion?: string,
+    });
+
+    loadDefinitions(): object;
+    createFunctionsFor(object?: any, apis?: any): void;
 }
 
-/*~ You can declare properties of the module using const, let, or var */
-export const myField: number;
+declare class API {
+    syno: Syno;
 
-/*~ If there are types, properties, or methods inside dotted names
- *~ of the module, declare them inside a 'namespace'.
- */
-export namespace subProp {
-    /*~ For example, given this definition, someone could write:
-     *~   import { subProp } from 'yourModule';
-     *~   subProp.foo();
-     *~ or
-     *~   import * as yourMod from 'yourModule';
-     *~   yourMod.subProp.foo();
-     */
-    function foo(): void;
+    constructor(syno?: Syno);
+
+    request(options?: {
+        api: string,
+        version: string,
+        path: string,
+        method: string,
+        params: object,
+    }, done?: void): any;
+
+    requestAPI(args?: {
+        params: object,
+        done: void,
+        apiInfos: {
+            api: string,
+            version: string,
+            path: string,
+            method: string,
+        },
+        requiredParams: Array<string>,
+    }): any;
+
+    error(code?: number): string;
+
+}
+
+declare class Auth extends API {
+    login(sessionName?: any, done?: any): any;
+
+    logout(sessionName?: any, done?: any): any;
+
+    error(code?: any): any;
+
+}
+
+declare class AuthenticatedAPI extends API {
+    request(options?: {
+        api: string,
+        version: string,
+        path: string,
+        method: string,
+        params: object,
+    }, done?: void): any;
+
+}
+
+import pluralize = require('pluralize');
+
+declare class Utils {
+    static underscoreToCamelize: any;
+    static trimSyno: any;
+    static trimSynoNamespace: any;
+    static fixCamelCase: any;
+    static deletePattern: any;
+    static listPluralize: any;
+    static createFunctionName: any;
+    static optionalParamsAndDone: any;
+}
+
+
+
+declare class VideoStation extends AuthenticatedAPI {
+    syno: any;
+    sessionName: any;
+    constructor(syno?: any);
+    getMethods(params?: any, done?: any): any;
+}
+
+
+declare class VideoStationDTV extends AuthenticatedAPI {
+    syno: any;
+    sessionName: any;
+    constructor(syno?: any);
+    getMethods(params?: any, done?: any): any;
+}
+
+declare class DownloadStation extends AuthenticatedAPI {
+    syno: any;
+    sessionName: any;
+    constructor(syno?: any);
+    getMethods(params?: any, done?: any): any;
+    error(code?: any, api?: any): any;
+}
+
+declare class DSM extends AuthenticatedAPI {
+    syno: any;
+    sessionName: any;
+    constructor(syno?: any);
+    getMethods(params?: any, done?: any): any;
+}
+
+declare class FileStation extends AuthenticatedAPI {
+    syno: any;
+    sessionName: any;
+    constructor(syno?: any);
+    getMethods(params?: any, done?: any): any;
+    error(code?: any, api?: any): any;
+}
+
+declare class SurveillanceStation extends AuthenticatedAPI {
+    syno: any;
+    sessionName: any;
+    constructor(syno?: any);
+    getMethods(params?: any, done?: any): any;
+    error(code?: any, api?: any): any;
+}
+
+declare class AudioStation extends AuthenticatedAPI {
+    syno: any;
+    sessionName: any;
+    constructor(syno?: any);
+    getMethods(params?: any, done?: any): any;
 }
